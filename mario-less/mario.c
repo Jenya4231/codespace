a39c8e4435b572302b10badcd5269c25a750a981
Jenya4231 <116454144+Jenya4231@users.noreply.github.com>
2023-10-23 17:32:36 +0000

    Mon, Oct 23, 2023, 5:32 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,11 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int size_input(void);
+
 int main(void)
 {
     char hash = '#';
 
-    int size = get_int("Size of pyramid: ");
+    int size = size_input();
 
     for (int n = 0; n < size; n++)
     {
@@ -21,5 +23,11 @@ int main(void)
 
 int size_input(void)
 {
-    
+    int n;
+    do
+    {
+        n = get_int("Size of pyramid: ");
+    }
+    while (n < 1 || n > 8);
+    return n;
 }
