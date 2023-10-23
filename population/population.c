138767ab3698db8cca7d55d908f03393b6dd0d4c
Jenya4231 <116454144+Jenya4231@users.noreply.github.com>
2023-10-23 09:01:28 +0000

    Mon, Oct 23, 2023, 9:01 AM +00:00

--- population/population.c
+++ population/population.c
@@ -1,13 +1,50 @@
 #include <cs50.h>
 #include <stdio.h>
 
+const int min_population_size = 9;
+
+int start_size;
+int end_size;
+
+int input_start(void);
+int input_end(void);
+
 int main(void)
 {
+
     // TODO: Prompt for start size
+    start_size = input_start();
 
     // TODO: Prompt for end size
+    end_size = input_end();
 
     // TODO: Calculate number of years until we reach threshold
 
     // TODO: Print number of years
+}
+
+int input_start(void)
+{
+    int n;
+
+    do
+    {
+        n = get_int("Start population size: ");
+    }
+    while (n <= min_population_size);
+
+    return n;
+}
+
+int input_end(void)
+{
+    int n;
+
+    do
+    {
+        n = get_int("End population size: ");
+    }
+    while (n <= start_size);
+
+    return n;
 }
